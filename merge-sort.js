be1c87a81145b41968e0fb60e709f4116c67c9a9
CopyypoC <william.wang650@gmail.com>
2025-04-19 16:05:13 -0700

    Clean logs and replace comments

--- merge-sort.js
+++ merge-sort.js
@@ -1,21 +1,22 @@
 function mergeSort(arr) {
-  console.log(`mergeSort([${arr}])`);
+  // Split into halves
   const left = arr.slice(0, Math.floor(arr.length / 2));
   const right = arr.slice(Math.floor(arr.length / 2));
   const sorted = [];
 
+  // Base case of single item
+  // slice takes (start, end) not including end so left is empty
   if (arr.length === 1) {
-    console.log(`return right: [${right}]`);
     return right;
   }
 
-  console.log(`sortedLeft = mergeSort([${left}])`);
+  // Recurse here to split and sort all the way down to 1 item
   const sortedLeft = mergeSort(left);
-  console.log(`sortedRight = mergeSort([${right}])`);
   const sortedRight = mergeSort(right);
 
   let i = 0;
   let j = 0;
+  // Sort and merge each item from the left and right halves
   while (i < sortedLeft.length && j < sortedRight.length) {
     if (sortedLeft[i] < sortedRight[j]) {
       sorted.push(sortedLeft[i]);
@@ -26,6 +27,7 @@ function mergeSort(arr) {
     }
   }
 
+  // Merge leftover items
   while (i < sortedLeft.length) {
     sorted.push(sortedLeft[i]);
     i++;
@@ -36,18 +38,10 @@ function mergeSort(arr) {
     j++;
   }
 
-  console.log(`return sorted: [${sorted}]`);
+  // Sorted array will build from the base case up to the final
+  // 2 halves because of the recursive calls
+  // i.e. sortedLeft and sortedRight start as single items
+  // and chain into the upper level sortedLeft and sortedRight
+  // until the entire recursive call chain is complete
   return sorted;
 }
-
-const arr = [3, 2, 1, 13, 8, 5, 0, 1];
-console.log(mergeSort(arr));
-
-// mergeSort([2, 1, 4])
-//  sortedLeft = mergeSort([2])
-//    return [2]
-//  sortedRight = mergeSort([1,4])
-//    sortedLeft = mergeSort([1])
-//      return [1]
-//    sortedRight = mergeSort([4])
-//      return [4]
